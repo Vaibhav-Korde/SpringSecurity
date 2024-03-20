ef4c99748ddf77a70923022dd16c1442c945d36d
Vaibhav Korde <tech.vaibhavk@gmail.com>
2024-03-20 22:13:21 +0530

    second commit

--- src/main/java/org/springsecurity/springsecurityexample/config/MySecurityConfig.java
+++ src/main/java/org/springsecurity/springsecurityexample/config/MySecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -20,6 +21,7 @@ public class MySecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((authorize) -> authorize
                         .requestMatchers("/home").permitAll()
                         .anyRequest().authenticated()
